adfc7f32dcf88bf0ba793a48e6d5f87dc833cdc8
JOSHLET4 <ecualetran@gmail.com>
2024-05-22 18:29:13 -0600

    Actividad en clase AHD2

---
+++ componente.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const headerContainer = document.querySelector("#header-container");
+
+    fetch("header.html")
+    .then(respuesta => {
+        if(!respuesta.ok) {
+            throw new Error("La respuesta no fue la correcta");
+        } return respuesta.text();
+    })
+    .then(datos => {
+        headerContainer.innerHTML = datos;
+    })
+});
+
+//funcion utilizar footer
+document.addEventListener("DOMContentLoaded", function() {
+    const footerContainer = document.querySelector("#footer-container");
+
+    fetch("footer.html")
+    .then(respuesta => {
+        if(!respuesta.ok) {
+            throw new Error("La respuesta no fue la correcta");
+        } return respuesta.text();
+    })
+    .then(datos => {
+        footerContainer.innerHTML = datos;
+    })
+});
